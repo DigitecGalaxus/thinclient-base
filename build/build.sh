52b8ee8ced2458f3031070bddfd0c9b754eaad53
mazlumtoprak <mazlumtoprak@gmail.com>
2021-09-27 16:55:19 +0200

    add arguments for build.sh as fallback

--- build/build.sh
+++ build/build.sh
@@ -5,29 +5,34 @@ set -e -o pipefail
 
 function removeFileIfExists {
         fileName="$1"
-        if [[ -f "$fileName" ]]
-        then
+        if [[ -f "$fileName" ]]; then
                 rm -f "$fileName"
         fi
 }
 
 # If there are less than six arguments passed to the build script (e.g. for local execution), try to set them automatically
-if [[ $# -lt 3 ]]
-then
-        echo "Warning: Passed less than 3 arguments. Ignoring all passed arguments and determining defaults"
-        branchName=$(git symbolic-ref -q --short HEAD)
-        branchName=${branchName:-HEAD}
-        gitCommitShortSha="$(git log -1 --pretty=format:%h)"
-        useDockerBuildCache="true"
+
+if [[ $# -lt 1 ]]; then
+        echo "Error: No arguments passed. Make sure to pass at least the Netboot IP Address"
 else
-        branchName="$1"
-        # In AzureDevOps it's only possible to pass the full commit sha - this is too long for us so we shorten it to 7 characters
-        gitCommitShortSha="${2:0:7}"
-        useDockerBuildCache="$3"
+        if [[ $# -lt 4 ]]; then
+                echo "Warning: Passed less than 4 arguments. Ignoring all passed arguments and determining defaults"
+                netbootIP="$1"
+                branchName=$(git symbolic-ref -q --short HEAD)
+                branchName=${branchName:-HEAD}
+                gitCommitShortSha="$(git log -1 --pretty=format:%h)"
+                useDockerBuildCache="true"
+        else
+                netbootIP="$1"
+                branchName="$2"
+                # In AzureDevOps it's only possible to pass the full commit sha - this is too long for us so we shorten it to 7 characters
+                gitCommitShortSha="${3:0:7}"
+                useDockerBuildCache="$4"
+
+        fi
 fi
 
-if [[ "$useDockerBuildCache" == "true" || "$useDockerBuildCache" == "True" ]]
-then
+if [[ "$useDockerBuildCache" == "true" || "$useDockerBuildCache" == "True" ]]; then
         dockerBuildCacheArgument=""
 else
         dockerBuildCacheArgument="--no-cache"
@@ -38,28 +43,27 @@ set -u
 
 imageName="anymodconrst001dg.azurecr.io/planetexpress/thinclient-base:21.04"
 
+# Name of the resulting squashfs file, e.g. 21-01-17-master-6d358edc.squashfs
+squashfsFilename="$(date +%y-%m-%d)-$branchName-$gitCommitShortSha.squashfs"
+
 # --no-cache is useful to apply the latest updates within an apt-get full-upgrade
 #docker image build --pull $dockerBuildCacheArgument -t "anymodconrst001dg.azurecr.io/planetexpress/thinclient-base:21.04" .
-docker image build --pull $dockerBuildCacheArgument -t "$imageName" .
-
+docker image build --build-arg OS_RELEASE=${squashfsFilename%.*} --build-arg NETBOOT_IP=$netbootIP --pull $dockerBuildCacheArgument -t "$imageName" .
 
 tarFileName="newfilesystem.tar"
 removeFileIfExists "$tarFileName"
 
-# Name of the resulting squashfs file, e.g. 21-01-17-master-6d358edc.squashfs
-squashfsFilename="$(date +%y-%m-%d)-$branchName-$gitCommitShortSha.squashfs"
-
 echo "Starting to tar container filesystem - this will take a while..."
 #containerID=$(docker container create "$imageName" tail /dev/null)
 containerID=$(docker run -d "$imageName" tail -f /dev/null)
-docker cp "$containerID:/" - > "$tarFileName"
+docker cp "$containerID:/" - >"$tarFileName"
 docker rm -f "$containerID"
 
 echo "Starting to convert tar file to squashfs file - this will take a while..."
 
 removeFileIfExists "$squashfsFilename"
 touch "$squashfsFilename"
-squashfsContainerID=$(docker run -d -u $(id -u)\
+squashfsContainerID=$(docker run -d -u $(id -u) \
         -v "$(pwd)/$tarFileName:/var/live/$tarFileName" \
         -v "$(pwd)/$squashfsFilename:/var/live/newfilesystem.squashfs" \
         anymodconrst001dg.azurecr.io/planetexpress/squashfs-tools:latest /bin/sh -c "tar2sqfs --force --quiet newfilesystem.squashfs < /var/live/$tarFileName")
