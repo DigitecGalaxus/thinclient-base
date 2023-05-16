08d46a4a42b621e88001a0aa32ce0029d2ca5a91
Christian Cadruvi <Christian.Cadruvi@digitecgalaxus.ch>
2023-05-16 15:02:59 +0200

    First draft of making promote optional

--- build/build.sh
+++ build/build.sh
@@ -10,25 +10,36 @@ function removeFileIfExists {
     fi
 }
 
-# If there are less than six arguments passed to the build script (e.g. for local execution), try to set them automatically
+# Parsing arguments passed to the build script which should be key=value pairs
+for ARGUMENT in "$@"; do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    KEY_LENGTH=${#KEY}
+    VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+    export "$KEY"="$VALUE"
+done
 
-if [[ $# -lt 1 ]]; then
-    echo "Error: No arguments passed. Make sure to pass at least the Netboot IP Address"
+if [[ "$netbootIP" == "" ]]; then
+    echo "Error: No arguments passed. Make sure to pass at least the Netboot IP Address, e.g. netbootIP=10.1.30.4"
 else
-    if [[ $# -lt 4 ]]; then
-        echo "Warning: Passed less than 4 arguments. Ignoring the last three passed arguments and determining defaults"
-        netbootIP="$1"
+    if [[ "$branchName" == "" ]]; then
         # To be consistent with the naming of the azure devops variable Build.SourceBranchName, we remove the prefixes containing slashes
         branchName=$(git symbolic-ref -q --short HEAD | rev | cut -d'/' --fields=1 | rev)
-        gitCommitShortSha="$(git log -1 --pretty=format:%h)"
-        useDockerBuildCache="true"
-    else
-        netbootIP="$1"
-        branchName="$2"
-        # In AzureDevOps it's only possible to pass the full commit sha - this is too long for us so we shorten it to 7 characters
-        gitCommitShortSha="${3:0:7}"
-        useDockerBuildCache="$4"
+        echo "Warning: No branch name passed. Using $branchName as branch name"
     fi
+    if [[ "$gitCommitShortSha" == "" ]]; then
+        gitCommitShortSha="$(git log -1 --pretty=format:%h)"
+        echo "Warning: No git commit short sha passed. Using $gitCommitShortSha as git commit short sha"
+    fi
+    if [[ "$useDockerBuildCache" == "" ]]; then
+        useDockerBuildCache="true"
+        echo "Warning: No useDockerBuildCache passed. Using $useDockerBuildCache as useDockerBuildCache"
+    fi
+    if [[ "$buildSquashfsAndPromote" == "" ]]; then
+        buildSquashfsAndPromote="false"
+        echo "Warning: No buildSquashfsAndPromote passed. Using $buildSquashfsAndPromote as buildSquashfsAndPromote"
+    fi
+    # In AzureDevOps it's only possible to pass the full commit sha - this is too long for us so we shorten it to 7 characters
+    gitCommitShortSha="${gitCommitShortSha:0:7}"
 fi
 
 if [[ "$useDockerBuildCache" == "true" || "$useDockerBuildCache" == "True" ]]; then
@@ -48,6 +59,11 @@ squashfsFilename="$(date +%y-%m-%d)-$branchName-$gitCommitShortSha.squashfs"
 
 # --no-cache is useful to apply the latest updates within an apt-get full-upgrade
 docker image build --build-arg OS_RELEASE=${squashfsFilename%.*} --build-arg NETBOOT_IP=$netbootIP $dockerBuildCacheArgument -t "$imageName" .
+
+if [[ "$buildSquashfsAndPromote" != "true" ]]; then
+    echo "Skipping squashfs build and promotion"
+    exit 0
+fi
 
 tarFileName="newfilesystem.tar"
 removeFileIfExists "$tarFileName"
@@ -71,5 +87,5 @@ docker rm -f "$squashfsContainerID"
 rm -f "$(pwd)/$tarFileName"
 
 squashfsAbsolutePath="$(pwd)/$squashfsFilename"
-# AzureDevOps specific way of passing an output variable to subsequent steps in the pipeline
-echo "##vso[task.setvariable variable=squashfsAbsolutePath;isOutput=true]$squashfsAbsolutePath"
+
+# Promote using https://jiradg.atlassian.net/browse/PSA-25794
