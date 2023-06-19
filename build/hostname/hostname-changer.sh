618c732b33d358c8ccd4895d4d8eb4f0ad92aea5
ccadruvi <christian.cadruvi@digitecgalaxus.ch>
2023-06-19 10:25:31 +0200

    Feature/planetexpress/psa 26110 shorten hostname (#23)
    * Update hostname-changer.sh
    
    * Update README.md
    
    * Update hostname-changer.sh

--- build/hostname/hostname-changer.sh
+++ build/hostname/hostname-changer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 # Extracting the last 6 digits from the MAC-adress of the first (head -1) (e*)thernet interface.
-macEnd=$(cat /sys/class/net/e*/address | sed 's/://g' | head -1)
-newHostname=tc-$macEnd
+mac=$(cat /sys/class/net/e*/address | sed 's/://g' | head -1)
+newHostname=tc-$mac
 echo "Setting hostname to $newHostname."
 sed -i "s/ubuntu/$newHostname/g" /etc/hosts
 sed -i "s/ubuntu/$newHostname/g" /etc/hostname
