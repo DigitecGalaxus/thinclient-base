1a7ed2f4082a5c277a0302b1f1f4a11b2bab81cb
Christian Cadruvi <christian.cadruvi@digitecgalaxus.ch>
2026-03-30 15:52:25 +0200

    Change hostname prefix to dg-tc, add ibt-1040 BT firmware, add apt-get update before firmware install (#60)

--- build/base-image/hostname/hostname-changer.sh
+++ build/base-image/hostname/hostname-changer.sh
@@ -2,7 +2,7 @@
 set -e
 # Extracting the last 6 digits from the MAC-adress of the first (head -1) (e*)thernet interface.
 mac=$(cat /sys/class/net/e*/address | sed 's/://g' | head -1)
-newHostname=tc-$mac
+newHostname=dg-tc-$mac
 echo "Setting hostname to $newHostname."
 sed -i "s/ubuntu/$newHostname/g" /etc/hosts
 sed -i "s/ubuntu/$newHostname/g" /etc/hostname
